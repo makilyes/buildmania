b85657239c24580b68ca8628ed90974ddd7a3b8d
makilyes <imakria@gmail.com>
2021-10-11 12:57:52 +0200

    video - addToCart Button - shadowPicture - Section FAQ

--- assets/global.js
+++ assets/global.js
@@ -723,3 +723,27 @@ class VariantRadios extends VariantSelects {
 }
 
 customElements.define('variant-radios', VariantRadios);
+
+
+// Custom JS
+
+//FAQ SECTION
+
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
