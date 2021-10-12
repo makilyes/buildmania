26d24a99db1f415cb2715e6acdf834ad6a2d48e3
mak_titi@hotmail.com <mak_titi@hotmail.com>
2021-10-12 22:13:15 +0200

    review caroussel updated

--- assets/global.js
+++ assets/global.js
@@ -729,21 +729,21 @@ customElements.define('variant-radios', VariantRadios);
 
 //FAQ SECTION
 
-let acc = document.getElementsByClassName("accordion");
-let i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    let panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
+// let acc = document.getElementsByClassName("accordion");
+// let i;
+//
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
+//     /* Toggle between adding and removing the "active" class,
+//     to highlight the button that controls the panel */
+//     this.classList.toggle("active");
+//
+//     /* Toggle between hiding and showing the active panel */
+//     let panel = this.nextElementSibling;
+//     if (panel.style.display === "block") {
+//       panel.style.display = "none";
+//     } else {
+//       panel.style.display = "block";
+//     }
+//   });
+// }
